33409b3f6641d770a205c5651b397adf17578e58
ZijiaLu <zz@server.com>
2016-06-24 19:45:22 +0800

    push

--- Net/CNN/cnn.py
+++ Net/CNN/cnn.py
@@ -29,18 +29,18 @@ def train():
 	train = out['train']
 	val   = out['val']
 
-	net = mx.sym.Reshape(data = n.bn10, target_shape = (0, 1*256*256))
-	net = mx.sym.FullyConnected(data = net, name = 'full1', num_hidden = 100)
-	net = mx.sym.FullyConnected(data = net, name = 'full2', num_hidden = 1*256*256)
+	# net = mx.sym.Reshape(data = n.bn10, target_shape = (0, 1*256*256))
+	# net = mx.sym.FullyConnected(data = net, name = 'full1', num_hidden = 100)
+	# net = mx.sym.FullyConnected(data = net, name = 'full2', num_hidden = 1*256*256)
 
 	# print net.infer_shape(data = (10,1,256,256))[1]
 
-	net = mx.sym.Reshape(data = net, target_shape = (0,1,256,256))
+	# net = mx.sym.Reshape(data = net, target_shape = (0,1,256,256))
 	
 	# print net.infer_shape(data = (10,1,256,256))[0]
 
 	# net = mx.sym.FullConnected()
-	net = mx.sym.Activation(data = net, act_type = 'sigmoid')
+	net = mx.sym.Activation(data = n.bn10, act_type = 'sigmoid')
 	net = mx.sym.Custom(data = net, name = 'softmax', op_type = 'sfmx')
 	# net = mx.sym.LogisticRegressionOutput(data = n.out, name = 'softmax')
 
@@ -52,21 +52,21 @@ def train():
 		net,
 		ctx = mx.context.gpu(1),
 		learning_rate = 1.5,
-		num_epoch = 100,
+		num_epoch = 10,
 		optimizer='adam', 
 		initializer = mx.initializer.Xavier(rnd_type = 'gaussian')
 	)
 
-	c = Callback()
+	c = Callback(draw_each=True)
 
 	if True:
 		model.fit(
 			train,
 			eval_data = val,
-			# eval_metric = mx.metric.create(eval_iou),
-			eval_metric = 'acc',
-			epoch_end_callback = c,
-			batch_end_callback = batch_end
+			eval_metric = mx.metric.create(c.eval),
+			# eval_metric = ,
+			epoch_end_callback = c.epoch,
+			batch_end_callback = c.batch,
 			)
 
 	else:
@@ -84,16 +84,15 @@ def train():
 	N = pred[0].shape[0]
 
 
-	for idx in range(N):
-		fig = plt.figure()
+	# for idx in range(N):
+	# 	fig = plt.figure()
 
-		img = pred[0][idx,0]
-		print '\n\nmean>>',img.mean(), 'std>>',img.std()
-		for num in range(3):
-			fig.add_subplot(131+num).imshow(pred[num][idx,0])
-		fig.savefig('Pred/%s-%d.png'%(c.name, idx))
-		fig.clear()
-
+	# 	img = pred[0][idx,0]
+	# 	print '\n\nmean>>',img.mean(), 'std>>',img.std()
+	# 	for num in range(3):
+	# 		fig.add_subplot(131+num).imshow(pred[num][idx,0])
+	# 	fig.savefig('Pred/%s-%d.png'%(c.name, idx))
+	# 	fig.clear()
 
 
 	c.all_to_png()
